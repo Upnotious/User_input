2549e6a2c543889c7367dd228772c3e012c5ffcf
Amogelang Loeto <160504907+Upnotious@users.noreply.github.com>
2024-08-18 10:14:56 +0200

    Assignment completed

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = input ("Please enter your age: ")
+location = input ("Please enter your location: ")
+
+print(f"Hey {name}, you are {age} and you live in {location}.")
